33c1db92464fbe7f536a7a7632a57eed7ddd7f04
Gopikrishna Sathyamurthy <sathyamurthygopikrishna@johndeere.com>
2018-06-13 17:05:51 -0500

    update the way they are all shown

--- src/strategy-printer.js
+++ src/strategy-printer.js
@@ -3,15 +3,41 @@ import React from 'react';
 import fs from 'fs';
 
 const strategies = {
-  'less-mixins': fs.readFileSync(__dirname + '/__raw_strategies/less-mixins.txt').toString(),
-  'pure-css': fs.readFileSync(__dirname + '/__raw_strategies/pure-css.txt').toString(),
-  'sass-hoc': fs.readFileSync(__dirname + '/__raw_strategies/sass-hoc.txt').toString(),
-  'sass-mixins': fs.readFileSync(__dirname + '/__raw_strategies/sass-mixins.txt').toString()
+  'less-mixins': fs.readFileSync(__dirname + '/__raw/less-mixins.txt').toString(),
+  'pure-css': fs.readFileSync(__dirname + '/__raw/pure-css.txt').toString(),
+  'sass-hoc': fs.readFileSync(__dirname + '/__raw/sass-hoc.txt').toString(),
+  'sass-mixins': fs.readFileSync(__dirname + '/__raw/sass-mixins.txt').toString()
 };
 
+const component = {__html: Prism.highlight(fs.readFileSync(__dirname + '/__raw/react-component.txt').toString(), Prism.languages.javascript, 'javascript')};
+const styleUsage = {__html: Prism.highlight(fs.readFileSync(__dirname + '/__raw/style-usage.txt').toString(), Prism.languages.css, 'css')};
+
 export const StrategyPrinter = props =>
-  <pre>
-    <code
-      dangerouslySetInnerHTML={{__html: Prism.highlight(strategies[props.strategy], Prism.languages.css, 'css')}}
-    />
-  </pre>;
+  <table>
+    <thead>
+      <tr>
+        <th>Strategy baseline</th>
+        <th>Usage</th>
+        <th>Component</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <pre>
+            <code dangerouslySetInnerHTML={{__html: Prism.highlight(strategies[props.strategy], Prism.languages.css, 'css')}}/>
+          </pre>
+        </td>
+        <td>
+          <pre>
+            <code dangerouslySetInnerHTML={styleUsage}/>
+          </pre>
+        </td>
+        <td>
+          <pre>
+            <code dangerouslySetInnerHTML={component}/>
+          </pre>
+        </td>
+      </tr>
+    </tbody>
+  </table>;
